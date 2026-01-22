80f49587c0213b466334c1a9be5f1b4fcb141fe3
El ouarroudi <zhaki.elouarroudi@gmail.com>
2026-01-22 11:11:25 +0100

    add: ajout des pages de la section visiteurs

--- src/Controller/Visitor/Blog/BlogController.php
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -12,7 +12,6 @@ final class BlogController extends AbstractController
     public function index(): Response
     {
         return $this->render('pages/visitor/blog/index.html.twig', [
-     
         ]);
     }
 }
